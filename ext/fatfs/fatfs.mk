c21c8087e1c19770bd9d95909cbfe08a67c801b4
Marian Such <toxygen1@gmail.com>
2013-04-24 14:54:03 +0200

    added fatfs

---
+++ ext/fatfs/fatfs.mk
@@ -0,0 +1,6 @@
+# FATFS files.
+FATFSSRC = ${CHIBIOS}/ext/fatfs/src/diskio.c \
+           ${CHIBIOS}/ext/fatfs/src/ff.c \
+           ${CHIBIOS}/ext/fatfs/src/option/syscall.c
+
+FATFSINC = ${CHIBIOS}/ext/fatfs/src
