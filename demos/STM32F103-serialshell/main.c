b90860d6ce879c5107c724775f19d72fde41502e
Marian Such <toxygen1@gmail.com>
2013-05-08 20:11:47 +0200

    new shell over serial

---
+++ demos/STM32F103-serialshell/main.c
@@ -0,0 +1,56 @@
+/* J76 jumper must be in place */
+
+#include <ch.h>
+#include <hal.h>
+#include "microshell2.h"
+
+/*===========================================================================*/
+/* Generic code.                                                             */
+/*===========================================================================*/
+
+static WORKING_AREA(waShell, 2048);
+
+/* shell thread */
+static msg_t shell(void *arg)
+{
+	(void) arg;
+	chRegSetThreadName("shell");
+	start_shell();
+}
+
+/* leds heartbeat */
+static WORKING_AREA(waThread1, 128);
+static msg_t Thread1(void *arg) {
+	
+	(void)arg;
+	chRegSetThreadName("blinker");
+	while (TRUE) {
+		palClearPad(GPIOB, GPIOB_LED1);
+		chThdSleepMilliseconds(500);
+		palSetPad(GPIOB, GPIOB_LED1);
+		chThdSleepMilliseconds(500);
+	}
+}
+
+int main(void) {
+	Thread *shelltp = NULL;
+	
+	halInit();
+	chSysInit();
+	
+	sdStart(&SD2, NULL);
+	
+	chThdCreateStatic(waThread1, sizeof(waThread1), NORMALPRIO, Thread1, NULL);
+
+	while (TRUE) {
+		if (!shelltp)
+		{
+			shelltp = chThdCreateStatic(waShell, sizeof(waShell),
+										NORMALPRIO, shell, NULL);
+		}
+		else if (chThdTerminated(shelltp)) {
+			shelltp = NULL;           /* Triggers spawning of a new shell.   */
+		}
+		chThdSleepMilliseconds(1000);
+	}
+}
---
+++ demos/STM32F103-serialshell/microshell2.h
@@ -0,0 +1,15 @@
+//
+//  microshell2.h
+//  
+//
+//  Created by Marian Such on 8/5/13.
+//
+//
+
+#ifndef _microshell2_h
+#define _microshell2_h
+
+void start_shell(void);
+
+
+#endif
