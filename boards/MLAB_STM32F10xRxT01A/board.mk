f9aba7b99151694694583301fd5356b5d2716d01
Jakub Kákona <kaklik@mlab.cz>
2013-07-05 11:11:46 +0200

    modifikace demo kodu na STM32F107

--- boards/MLAB_STM32F10xRxT01A/board.mk
+++
@@ -1,5 +0,0 @@
-# List of all the board related files.
-BOARDSRC = ${CHIBIOS}/boards/MLAB_STM32F10xRxT01A/board.c
-
-# Required include directories
-BOARDINC = ${CHIBIOS}/boards/MLAB_STM32F10xRxT01A
